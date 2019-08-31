f35748550be4e03b60dd6c3dcedcc162e1c19b67
raymond <iamsemyung@gmail.com>
2019-08-31 16:03:52 +0900

    Team changed
    Team changed

--- team.js
+++ team.js
@@ -1,12 +1,14 @@
 function init() {
   const slides = document.querySelectorAll(".slide");
-  const pages = document.querySelectorAll(".page");
+  const pages = document.querySelectorAll(".team");
   const backgrounds = [
+    `radial-gradient(#2B3760, #0B1023)`,
+    `radial-gradient(#4E3022, #161616)`,
     `radial-gradient(#2B3760, #0B1023)`,
     `radial-gradient(#4E3022, #161616)`,
     `radial-gradient(#2B3760, #0B1023)`
   ];
-  //Tracker
+
   let current = 0;
   let scrollSlide = 0;
 
@@ -28,28 +30,15 @@ function init() {
   function nextSlide(pageNumber) {
     const nextPage = pages[pageNumber];
     const currentPage = pages[current];
-    const nextLeft = nextPage.querySelector(".hero .model-left");
-    const nextRight = nextPage.querySelector(".hero .model-right");
-    const currentLeft = currentPage.querySelector(".hero .model-left");
-    const currentRight = currentPage.querySelector(".hero .model-right");
-    const nextText = nextPage.querySelector(".details");
-    const portofolio = document.querySelector(".portofolio");
+    const nextPic = nextPage.querySelector(".team__picture");
+    const currentPic = currentPage.querySelector(".team__picture");
+    const nextText = nextPage.querySelector(".team__name");
+    const portofolio = document.querySelector(".team");
 
-    const tl = new TimelineMax({
-      onStart: function() {
-        slides.forEach(slide => {
-          slide.style.pointerEvents = "none";
-        });
-      },
-      onComplete: function() {
-        slides.forEach(slide => {
-          slide.style.pointerEvents = "all";
-        });
-      }
-    });
+    const tl = new TimelineMax({});
 
-    tl.fromTo(currentLeft, 0.3, { y: "-10%" }, { y: "-100%" })
-      .fromTo(currentRight, 0.3, { y: "10%" }, { y: "-100%" }, "-=0.2")
+    tl.fromTo(currentPic, 0.3, { y: "-10%" }, { y: "-100%" })
+
       .to(portofolio, 0.3, { backgroundImage: backgrounds[pageNumber] })
       .fromTo(
         currentPage,
@@ -64,43 +53,11 @@ function init() {
         { opacity: 1, pointerEvents: "all" },
         "-=0.6"
       )
-      .fromTo(nextLeft, 0.3, { y: "-100%" }, { y: "-10%" }, "-=0.6")
-      .fromTo(nextRight, 0.3, { y: "-100%" }, { y: "10%" }, "-=0.8")
-      .fromTo(nextText, 0.3, { opacity: 0, y: 0 }, { opacity: 1, y: 0 })
-      .set(nextLeft, { clearProps: "all" })
-      .set(nextRight, { clearProps: "all" });
+      .fromTo(nextPic, 0.3, { y: "-100%" }, { y: "-10%" }, "-=0.6")
+
+      .fromTo(nextText, 0.3, { opacity: 0, y: 0 }, { opacity: 1, y: 0 });
 
     current = pageNumber;
-  }
-
-  //OPTIONAL
-  document.addEventListener("wheel", throttle(scrollChange, 1500));
-  document.addEventListener("touchmove", throttle(scrollChange, 1500));
-
-  function switchDots(dotNumber) {
-    const activeDot = document.querySelectorAll(".slide")[dotNumber];
-    slides.forEach(slide => {
-      slide.classList.remove("active");
-    });
-    activeDot.classList.add("active");
-  }
-
-  function scrollChange(e) {
-    if (e.deltaY > 0) {
-      scrollSlide += 1;
-    } else {
-      scrollSlide -= 1;
-    }
-
-    if (scrollSlide > 2) {
-      scrollSlide = 0;
-    }
-    if (scrollSlide < 0) {
-      scrollSlide = 4;
-    }
-    switchDots(scrollSlide);
-    nextSlide(scrollSlide);
-    console.log(scrollSlide);
   }
 }
 
